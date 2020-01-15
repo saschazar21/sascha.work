335b2e2c2118c708052c97ec558b36f50ec23c1c
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-01-15 18:07:54 +0100

    fix(html): added lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,26 @@
 import React, { ReactElement } from 'react';
-import Document, { DocumentInitialProps, DocumentContext } from 'next/document';
+import Document, {
+  DocumentInitialProps,
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components/macro';
 import { RenderPageResult } from 'next/dist/next-server/lib/utils';
 
 export default class WebsiteDocument extends Document {
   static async getInitialProps(
-    ctx: DocumentContext
+    ctx: DocumentContext,
   ): Promise<DocumentInitialProps> {
     const { renderPage } = ctx || {};
     const sheet = new ServerStyleSheet();
 
     ctx.renderPage = (): RenderPageResult =>
       renderPage({
-        enhanceApp: App => (props): ReactElement =>
-          sheet.collectStyles(<App {...props} />)
+        enhanceApp: (App) => (props): ReactElement =>
+          sheet.collectStyles(<App {...props} />),
       }) as RenderPageResult;
 
     try {
@@ -25,10 +32,21 @@ export default class WebsiteDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        )
+        ),
       });
     } finally {
       sheet.seal();
     }
+  }
+
+  public render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <Main />
+          <NextScript />
+        </Head>
+      </Html>
+    );
   }
 }
