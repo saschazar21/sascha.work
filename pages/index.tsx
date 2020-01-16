330507fa74c4740f6199908e6204cf8d75ece5fe
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-01-16 12:34:54 +0100

    feat(index): added profile image animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components/macro';
 
 import Title from 'components/title';
 import FullBleed from 'components/fullbleed';
@@ -7,12 +8,36 @@ import ProfileImage from 'components/profile/image';
 
 export const config = { amp: true };
 
+const drop = keyframes`
+  from {
+    opacity: 0.0;
+    transform: translateY(-100%);
+  }
+  68% {
+    transform: translateY(12%);
+  }
+  85% {
+    transform: translateY(-6%)
+  }
+  96% {
+    transform: translateY(3%);
+  }
+  to {
+    opacity: 1.0;
+    transform: translateY(0);
+  }
+`;
+
+const ProfileImageAnimated = styled(ProfileImage)`
+  animation: ${drop} 450ms ease-in;
+`;
+
 const Index = (): JSX.Element => {
   return (
     <main>
       <FullBleed data-color="grey">
         <Container>
-          <ProfileImage />
+          <ProfileImageAnimated />
           <Title>hello, world!</Title>
           <p>
             I&apos;m <strong>Sascha</strong> – a web developer from Austria, and
