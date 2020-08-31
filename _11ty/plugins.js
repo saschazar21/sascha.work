523d1e131b721ea46122800cde1436ffbf0839f6
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-08-31 11:20:23 +0200

    feat: added JSON Feed

--- _11ty/plugins.js
+++ _11ty/plugins.js
@@ -1,8 +1,19 @@
 const ampPlugin = require('@ampproject/eleventy-plugin-amp');
 const helmetPlugin = require('eleventy-plugin-helmet');
+const { configFunction: jsonFeedPlugin } = require('eleventy-plugin-json-feed');
 const pwaPlugin = require('eleventy-plugin-pwa');
 
 module.exports = [
+  [
+    jsonFeedPlugin,
+    {
+      banner_image_metadata_field_name: 'cover',
+      content_html: true,
+      image_metadata_field_name: 'image',
+      summary_metadata_field_name: 'tldr',
+      tags_metadata_field_name: 'tags',
+    },
+  ],
   [helmetPlugin, {}],
   [
     ampPlugin,
---
+++ _data/feed.js
@@ -0,0 +1,21 @@
+const { absoluteUrl } = require('../_11ty/filters');
+
+const pkg = require('../package.json');
+const manifest = require('./manifest.json');
+
+module.exports = {
+  title: manifest.name,
+  home_page_url: absoluteUrl(),
+  description: manifest.description,
+  feed_url: absoluteUrl('/feed.json'),
+  icon: absoluteUrl(manifest.icons[1].src),
+  favicon: absoluteUrl('/favicon.ico'),
+  author: {
+    name: pkg.author.name,
+    url: pkg.author.url,
+    avatar: absoluteUrl(
+      'https://avatars2.githubusercontent.com/u/9016897?s=460'
+    ),
+  },
+  expired: false,
+};
