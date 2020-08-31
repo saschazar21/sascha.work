e04b85eb35383c0d7d1faaf90339d46aff684f66
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-08-31 14:56:02 +0200

    feat: added RSS feed

--- _11ty/plugins.js
+++ _11ty/plugins.js
@@ -2,6 +2,7 @@ const ampPlugin = require('@ampproject/eleventy-plugin-amp');
 const helmetPlugin = require('eleventy-plugin-helmet');
 const { configFunction: jsonFeedPlugin } = require('eleventy-plugin-json-feed');
 const pwaPlugin = require('eleventy-plugin-pwa');
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
 
 module.exports = [
   [
@@ -11,20 +12,21 @@ module.exports = [
       content_html: true,
       image_metadata_field_name: 'image',
       summary_metadata_field_name: 'tldr',
-      tags_metadata_field_name: 'tags',
-    },
+      tags_metadata_field_name: 'tags'
+    }
   ],
+  [rssPlugin, {}],
   [helmetPlugin, {}],
   [
     ampPlugin,
     {
-      filter: /(?<!serviceworker)\/index\.html/i,
-    },
+      filter: /(?<!serviceworker)\/index\.html/i
+    }
   ],
   [
     pwaPlugin,
     {
-      swDest: 'out/sw.js',
-    },
-  ],
+      swDest: 'out/sw.js'
+    }
+  ]
 ];
