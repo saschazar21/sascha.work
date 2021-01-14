70efa5227eef1a7e21fdbb0e2879d8ed057d6ef5
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2021-01-14 11:28:07 +0100

    feat: added tags page, added font files

--- _11ty/collections.js
+++ _11ty/collections.js
@@ -1,6 +1,21 @@
 const isProd = process.env.NODE_ENV === 'production';
 
+const publicPosts = collection =>
+  collection.getFilteredByTag('post').filter(p => !isProd || p.data.public);
+
 module.exports = {
-  public: collection =>
-    collection.getFilteredByTag('post').filter(p => !isProd || p.data.public)
+  public: publicPosts,
+  tags: collection =>
+    Array.from(
+      publicPosts(collection)
+        .reduce((map, current) => {
+          current.data.tags.forEach(
+            tag =>
+              tag !== 'post' &&
+              map.set(tag, [].concat(map.get(tag) || [], [current]))
+          );
+          return map;
+        }, new Map())
+        .entries()
+    )
 };
