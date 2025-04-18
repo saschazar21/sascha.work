56238085f16f114777f7c9d9961a34d4b20ff936
Sascha Zarhuber <saschazar21@yahoo.de>
2025-04-18 07:14:40 +0200

    feat: added Service Worker

--- _11ty/bundles.js
+++ _11ty/bundles.js
@@ -1,6 +1,22 @@
 import { join } from 'path';
 import { build, transform } from 'esbuild';
 
+const REPLACEMENTS = {
+  __HOSTNAME__: JSON.stringify(
+    new URL(
+      process.env.CONTEXT === 'production'
+        ? process.env.URL
+        : 'http://localhost:8080',
+    ).hostname,
+  ),
+  __IS_PROD__: process.env.CONTEXT === 'production',
+  __VERSION__: JSON.stringify(
+    process.env.CONTEXT === 'production'
+      ? process.env.VERSION || process.env.SHORT_SHA
+      : 'dev',
+  ),
+};
+
 export default {
   css: {
     outputFileExtension: 'css',
@@ -26,11 +42,18 @@ export default {
             loader: 'ts',
           },
           bundle: true,
+          format: 'esm',
+          treeShaking: process.env.CONTEXT === 'production',
           write: false,
         });
 
         const concatenated = bundle.outputFiles.reduce((result, { text }) => {
-          result += text;
+          let replaced = text;
+          for (const [key, value] of Object.entries(REPLACEMENTS)) {
+            replaced = replaced.replace(new RegExp(key, 'g'), value);
+          }
+
+          result += replaced;
           return result;
         }, '');
 
--- _11ty/transforms.js
+++ _11ty/transforms.js
@@ -1,7 +1,10 @@
 export default {
   purgeEmptyParagraphs(content) {
     return (this.page.outputPath ?? '').endsWith('.html')
-      ? content.replace(/\s*<p><\/p>\s*/g, '').replace(/\s*\n\s*\n\s*/g, '')
-      : content;
+      ? content
+          .trim()
+          .replace(/\s*<p><\/p>\s*/g, '')
+          .replace(/\s*\n\s*\n\s*/g, '')
+      : content.trim();
   },
 };
